5d5c684694f92b0311f9573dfcf6ee41350cd50f
Juan Cristobal <juancristobal@Juans-MacBook-Pro.local>
2026-06-16 21:59:00 -0400

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import { useAuthStore } from "@/stores/auth-store";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api";
+const API_URL = " https://chinident-api.argoz.cl/api";
 
 class ApiClient {
   private baseUrl: string;
